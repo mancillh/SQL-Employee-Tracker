41669998e465e4357d2a11ddea53013095b5319e
LeonathaTurtle <mancillh@gmail.com>
2024-08-11 13:55:59 -0500

    created server file and three sql files (query, schema, seeds), wrote content for schema.sql based on image provided in challenge instructions

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+\c company_db;
+
+CREATE TABLE department (
+  id SERIAL PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    role_title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department INT NOT NULL,
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    FOREIGN KEY (id)
+    REFERENCES employee(manager_id)
+    ON DELETE SET NULL
+);
